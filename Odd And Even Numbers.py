0a11e00ef677412c9abed1d82ae6a65967a74bb9
OmarMohamedAbdelghaffar <127775449+OmarMohamedAbdelghaffar@users.noreply.github.com>
2023-03-13 20:47:16 +0400

    Add files via upload

---
+++ Odd And Even Numbers.py
@@ -0,0 +1,7 @@
+#Number=int(input("Please Enter Your Number ---->  "))
+for Number in range (11) :
+
+    if Number%2==0 :
+        print(Number,"Is Even")
+    else :
+        print(Number,"Is Odd")
